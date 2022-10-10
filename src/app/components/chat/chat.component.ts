8a75dd652d0421809162f0e12e1ed7da4b278039
MGamezG <l171130314@gamadero.tecnm.mx>
2022-10-10 18:45:37 -0500

    desplegado de mansajes

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -7,20 +7,42 @@ import { ChatService } from 'src/app/providers/chatService/chat.service';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
-  mensaje!:string
-  constructor(public chatService:ChatService) {
+  mensaje!: string
+  elementScroll:any;
+  constructor(public chatService: ChatService) {
     this.chatService.cargarMensaje().subscribe(
-      (data:any[])=>{
-        console.log(data)
+      // (data: any[]) => {
+      //   console.log(data)
+      // }
+      ()=>{
+        setTimeout(()=>{
+          this.elementScroll.scrollTop=this.elementScroll.scrollHeight;
+        },20)
+
       }
     )
   }
 
 
   ngOnInit(): void {
+    this.elementScroll=document.getElementById('app-mensajes')
   }
-  enviar_mensaje(){
-    console.log(this.mensaje)
+  enviar_mensaje() {
+    if(this.mensaje.length==0){
+      return;
+    }else{
+      this.chatService.addMensaje(this.mensaje).then(
+        ()=>{
+          console.log('Mensaje guardado:',this.mensaje)
+        }
+      ).catch(
+        (err)=>{
+          console.log('error al enviar',err)
+        }
+      );
+
+    }
+
   }
 
 }
--- src/app/providers/chatService/chat.service.ts
+++ src/app/providers/chatService/chat.service.ts
@@ -1,19 +1,41 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
-import {Observable} from 'rxjs'
+import { Observable,map  } from 'rxjs'
+import { Mensaje } from 'src/app/interfaces/mensaje.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
-  chats:any[]=[]
-  private itemsCollection!:AngularFirestoreCollection<any>
-  items!:Observable<any[]>
-  constructor(private afs:AngularFirestore) { }
+  chats:Mensaje[] = []
+  private itemsCollection!: AngularFirestoreCollection<Mensaje>
+  items!: Observable<Mensaje[]>
+  constructor(private afs: AngularFirestore) { }
 
   cargarMensaje(){
-  this.itemsCollection=this.afs.collection<any>('chats');
+    this.itemsCollection = this.afs.collection<Mensaje>('chats',ref=>ref.orderBy('fecha', 'desc').limit(5));
 
-  return this.itemsCollection.valueChanges();
+    return this.itemsCollection.valueChanges().pipe(
+      map(
+        (mensajes:Mensaje[])=>{
+          //this.chats=mensajes
+          this.chats=[]
+          for (let mensaje of mensajes) {
+            this.chats.unshift(mensaje);
+          }
+          console.log(this.chats)
+         return this.chats
+        }
+      )
+    )
+  }
+  addMensaje(texto:string){
+    let mensaje:Mensaje={
+      //TODO:falta el Uid del user
+      nombre:'mg',
+      mensaje:texto,
+      fecha:new Date().getTime()
+    }
+    return this.itemsCollection.add(mensaje);
   }
 }
